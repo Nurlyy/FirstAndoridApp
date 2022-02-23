11f3e8df9de70ab68fa60d6ec4b8f6e956abfbf2
Nurlytan <nurik.bro.2000@mail.ru>
2022-02-23 23:41:35 +0600

    Creating Adapter and data class for Task object. Connecting an Adapter to RecyclerView

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.myfirstapplication
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myfirstapplication.databinding.ActivityMainBinding
 import java.math.BigDecimal
 
@@ -13,5 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val recyclerView = binding.recyclerView
+
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+
+        val tasks = ArrayList<Task>()
+
+        tasks.add(Task("Read a book", "read a book Steve Jobs by Walter Isaacson"))
+        tasks.add(Task("Learn Kotlin", "Learn more about RecyclerView in Kotlin"))
+        tasks.add(Task("Have a dinner" ))
+        tasks.add(Task(taskDescription = "wash dishes"))
+
+        val adapter = CustomAdapter(tasks)
+
+        recyclerView.adapter = adapter
     }
 }
